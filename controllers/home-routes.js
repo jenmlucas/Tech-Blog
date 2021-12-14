e7c66617b88bab2ee0e965d42aecd6ceacd5450f
jenmlucas <jenmlucas1@gmail.com>
2021-12-14 16:42:49 -0700

    created homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,85 @@
 const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-      res.render("homepage")
-});
+  console.log(req.session);
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_text",
+      "title",
+      "created_at",
+    
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      // pass a single post object into the homepage template
+      res.render("homepage", { 
+        posts,
+      loggedIn: req.session.loggedIn
+     });
+    })
+    .catch((err) => res.json(err));
+    });
+
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  
   res.render("login");
 });
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_text',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+    })
+    .catch(err => res.json(err));
+    });
+
 module.exports = router;
