cf996e9c46aebcc81ae6dd472695af768cdfeb09
jenmlucas <jenmlucas1@gmail.com>
2021-12-13 19:57:05 -0700

    wrote models

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- This is used to establish the data base that this site is going to use. The Drop database makes it so there is no duplication of information when starting the database over and over. 
+DROP DATABASE IF EXISTS tech_blog_db;
+
+CREATE DATABASE tech_blog_db;
